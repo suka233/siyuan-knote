dcde3bdbeb6993ab3fff0cbe35abad3775511cbe
Song <suka233@outlook.com>
2023-12-07 15:05:49 +0800

    fix: 修复刚开始的时候，没有自动查询出数据的bug

--- src/components/knoteDock/App.vue
+++ src/components/knoteDock/App.vue
@@ -10,10 +10,22 @@ import '@mdi/font/css/materialdesignicons.css'
 import HomeView from './src/views/home/index.vue'
 import { useData } from './src/hooks/useData'
 import { watch } from 'vue'
-const { getConfig, saveConfig, dailyNotebookId, displayMode, getTargetDailyDocId, selectedDay, useNewQuery } = useData()
+const {
+  getConfig,
+  saveConfig,
+  dailyNotebookId,
+  displayMode,
+  getTargetDailyDocId,
+  selectedDay,
+  useNewQuery,
+  refreshSiyuanKnotes
+} = useData()
 getConfig().then(() => {
   // console.log(res)
   getTargetDailyDocId(selectedDay.value)
+  setTimeout(() => {
+    refreshSiyuanKnotes()
+  })
 })
 
 // 自动保存
